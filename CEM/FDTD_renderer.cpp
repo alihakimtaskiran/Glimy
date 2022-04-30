5d9c7deb026239ec4db8b12acd1762127012893a
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2022-04-30 15:59:24 +0300

    Add files via upload

---
+++ CEM/FDTD_renderer.cpp
@@ -0,0 +1,44 @@
+#include<fstream>
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+
+void print(string arg){
+
+    cout<<arg<<endl;
+}
+void print(double arg){
+        cout<<arg<<endl;
+
+}
+
+
+int main(){
+    fstream io;
+    string read;
+
+    io.open("structure.tgf");
+    getline(io,read);
+    getline(io,read);
+
+    static uint8_t dim=stoi(read);
+    getline(io,read);
+    static bool curvilinear=false;
+    if (read=="1")
+    {
+        curvilinear=true;
+    }
+    getline(io,read);
+    static double deltaS=stod(read);    
+    getline(io,read);
+    static double deltaT=stod(read);
+    //the preamble read
+
+    
+
+    io.close();
+
+    return 0;
+
+}
