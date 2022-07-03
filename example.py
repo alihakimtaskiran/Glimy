8e8b12b3e4597e06034c86c7c64ebe6f51cc5be8
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2022-07-03 19:23:05 +0300

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import CEM
 field=CEM.Continuum(2, (100,100), 10e-9)
 
 a=CEM.geo2D.Rectangle((0,0), (99,99), 100,1)
+
 field.add(a)
 
 for i in range(10):
@@ -11,7 +12,7 @@ for i in range(10):
         field.add(CEM.geo2D.Circle((4+10*i,4+10*j), r=3, layer=8, e=10))
 
 
-field.add_energizer(CEM.DotSource((45,45), (0,100), 1, CEM.c/(50e-9)))
+field.add_energizer(CEM.DotSource((45,45), (0,100), 1, CEM.c/(120e-9)))
 field.view_structure(False)
 
 CEM.Render(field, 100)
