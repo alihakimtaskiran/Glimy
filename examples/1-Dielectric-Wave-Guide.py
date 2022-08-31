9694b6010116d63b2cf00851b42893d59504d25e
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2022-08-31 10:18:51 +0300

    Add files via upload

--- examples/1-Dielectric-Wave-Guide.py
+++ examples/1-Dielectric-Wave-Guide.py
@@ -1,20 +1,20 @@
-import CEM
-field=CEM.Continuum(2, (100,100), 10e-9)
+import glimy
+field=glimy.Continuum(2, (100,100), 10e-9)
 
-a=CEM.geo2D.Rectangle((0,0), (99,99), 100,1)
+a=glimy.geo2D.Rectangle((0,0), (99,99), 100,1)
 
 field.add(a)
 
 for i in range(10):
     for j in range(4):
-        field.add(CEM.geo2D.Circle((4+10*i,4+10*j), r=3, layer=8, e=10))
+        field.add(glimy.geo2D.Circle((4+10*i,4+10*j), r=3, layer=8, e=10))
     for j in range(5,10):
-        field.add(CEM.geo2D.Circle((4+10*i,4+10*j), r=3, layer=8, e=10))
+        field.add(glimy.geo2D.Circle((4+10*i,4+10*j), r=3, layer=8, e=10))
 
 
-field.add_energizer(CEM.DotSource((45,45), (0,100), 1, CEM.c/(120e-9)))
+field.add_energizer(glimy.DotSource((45,45), (0,100), 1, glimy.c/(120e-9)))
 field.view_structure(False)
 
-CEM.Render(field, 100)
+glimy.Render(field, 100)
 
 field.view_field()
--- examples/2-Near-Massive-Object.py
+++ examples/2-Near-Massive-Object.py
@@ -1,13 +1,13 @@
-import CEM
+import glimy
 
-obj=CEM.curved.SingularCelestial((2,4), 1e27)
+obj=glimy.curved.SingularCelestial((2,4), 1e27)
 
-field=CEM.Continuum(2,(100,100), 1e-2)
+field=glimy.Continuum(2,(100,100), 1e-2)
 field.set_curve(True)
 field.add(obj)
 
 field.view_structure(False)
-field.add_energizer(CEM.DotSource((10,10), (0,1000), 1, 2.4e9))
-CEM.Render(field,500)
+field.add_energizer(glimy.DotSource((10,10), (0,1000), 1, 2.4e9))
+glimy.Render(field,500)
 
 field.view_field()
