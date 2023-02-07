7d82855531e3c98f12e2ee3e093d8af89027661f
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2023-02-07 19:01:50 +0300

    Update 1-Dielectric-Wave-Guide.py

--- examples/1-Dielectric-Wave-Guide.py
+++ examples/1-Dielectric-Wave-Guide.py
@@ -1,20 +1,21 @@
 import glimy
-field=glimy.Continuum(2, (100,100), 10e-9)
+field=glimy.Continuum((100,100), 10e-9)
 
-a=glimy.geo2D.Rectangle((0,0), (99,99), 100,1)
+a=glimy.geo.Rectangle((0,0), (99,99), 100,1)
 
 field.add(a)
 
 for i in range(10):
     for j in range(4):
-        field.add(glimy.geo2D.Circle((4+10*i,4+10*j), r=3, layer=8, e=10))
+        field.add(glimy.geo.Circle((4+10*i,4+10*j), r=3, layer=8, e=10))
     for j in range(5,10):
-        field.add(glimy.geo2D.Circle((4+10*i,4+10*j), r=3, layer=8, e=10))
+        field.add(glimy.geo.Circle((4+10*i,4+10*j), r=3, layer=8, e=10))
 
 
-field.add_energizer(glimy.DotSource((45,45), (0,100), 1, glimy.c/(120e-9)))
-field.view_structure(False)
+field.add(glimy.DotSource((45,45), (0,100), 1, glimy.c/(120e-9)))
+field.build()
+field.view_structure()
 
-glimy.Render(field, 100)
+field.Render(100)
 
 field.view_field()
