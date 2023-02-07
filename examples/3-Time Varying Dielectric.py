621fc176b463fe45f2c99268c191297e16993b35
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2023-02-07 19:09:08 +0300

    Update 3-Time Varying Dielectric.py

--- examples/3-Time Varying Dielectric.py
+++ examples/3-Time Varying Dielectric.py
@@ -1,26 +1,28 @@
 import glimy
 
-encoder=glimy.Continuum(2, (20,300), 5e-9)
+encoder=glimy.Continuum((20,300), 5e-9)
 
 DBR=[]
 
 for i in range(5):
-    DBR.append(glimy.geo2D.Rectangle((0,18*i), (20,18*i+8),0,5))#n=5 d=40 nm
-    DBR.append(glimy.geo2D.Rectangle((0,18*i+8), (20,18*i+18),5,40))#n=4 d=50 nm
+    DBR.append(glimy.geo.Rectangle((0,18*i), (20,18*i+8),0,5))#n=5 d=40 nm
+    DBR.append(glimy.geo.Rectangle((0,18*i+8), (20,18*i+18),5,40))#n=4 d=50 nm
 
 msg=[
-     glimy.geo2D.VRectangle((0,100),(19,150), 50,0, 30),
-     glimy.geo2D.VRectangle((0,100),(19,150), 100,0, 2),
-     glimy.geo2D.VRectangle((0,100),(19,150), 150,0, 30),
-     glimy.geo2D.VRectangle((0,100),(19,150), 200,0, 2)
+     glimy.geo.Rectangle((0,100),(19,150),0, 30,time=(50,10000000)),
+     glimy.geo.Rectangle((0,100),(19,150),0, 2,time=(100,100000002)),
+     glimy.geo.Rectangle((0,100),(19,150),0, 30,time=(150,100000003)),
+     glimy.geo.Rectangle((0,100),(19,150),0, 2,time=(200,100000004))
      ]
 
 encoder.add((DBR,msg))
 
-encoder.add_energizer(glimy.DotSource((10,72), (0,100000), 1, 3.747405725e+14))
+encoder.add(glimy.DotSource((10,72), (0,100000), 1, 3.747405725e+14))
 
-encoder.view_structure(False)
+encoder.build()
+
+encoder.view_structure()
 
 
-glimy.Render(encoder, 1000)
+encoder.Render(1000)
 encoder.view_field()
