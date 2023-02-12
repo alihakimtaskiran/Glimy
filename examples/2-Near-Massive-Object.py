bca6c3f96ada021bc3116029d577e4b580923ed3
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2023-02-12 15:01:40 +0300

    Update 2-Near-Massive-Object.py

--- examples/2-Near-Massive-Object.py
+++ examples/2-Near-Massive-Object.py
@@ -1,15 +1,15 @@
 import glimy
 
-obj=glimy.geo.SingularCelestial((2,4), 1e28)
+obj=glimy.geo.SingularCelestial((.3,.2), 1e27)
 
 field=glimy.Continuum((100,100), 1e-2)
 field.add(obj)
 
-field.view_metric()
-field.add(glimy.DotSource((10,10), (0,1000), 1, 2.4e9))
+field.add(glimy.DotSource((50,20), (0,1000), 1, 2.4e9))
 
 field.build()
+field.view_metric()
 
-field.Render(500)
+field.Render(250)
 
 field.view_field()
