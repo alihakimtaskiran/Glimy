328151e41e19a8b23bff591efd9c4fe46f7afea1
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2023-03-09 15:16:12 +0300

    Add files via upload

---
+++ examples/6-Lossy Medium.py
@@ -0,0 +1,12 @@
+import glimy
+
+conduct=glimy.geo.RectPrism((10,10,10),(100,50,100),e=1,sigma=7000)
+grid=glimy.Continuum((100,100,100),5e-8)
+grid.add(conduct)
+grid.add(glimy.DotSource((50,50,50),[0,1000],1, 5e14))
+
+grid.build()
+grid.view_structure("sigma","z",62)
+grid.Render(100)
+grid.view_field('E','z',62)
+grid.view_field('J','z',62)
