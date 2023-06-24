03c0840850ed5b45d3b535f4c2b05df323c41ae7
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2023-06-25 00:39:09 +0300

    Update 7-GPU.py

--- examples/7-GPU.py
+++ examples/7-GPU.py
@@ -5,8 +5,8 @@ grid=glimy.Continuum((100,100,100),5e-8)
 grid.add(conduct)
 grid.add(glimy.DotSource((50,50,50),[0,1000],1, 5e14))
 
-grid.build(gpu_flag=1)
+grid.build(gpu_flag=1)#IMPORTANT: Set gpu_flag=1
 grid.view_structure("sigma","z",62)
-grid.Render(100,"cupy")
+grid.Render(100,"cupy")#IMPORTant: Set backend "cupy"
 grid.view_field('E','z',62)
 grid.view_field('J','z',62)
